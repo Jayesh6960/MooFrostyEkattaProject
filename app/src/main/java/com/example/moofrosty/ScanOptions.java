bca0bb4882c176078dacb435ff7a8b8159fa58e9
Prathamsj12 <prathmesh.jangam80@gmail.com>
2025-11-20 15:22:40 +0530

    Scanner Code updated in the code

---
+++ app/src/main/java/com/example/moofrosty/ScanOptions.java
@@ -0,0 +1,16 @@
+package com.example.moofrosty;
+
+public class ScanOptions {
+    public void setPrompt(String s) {
+    }
+
+    public void setBeepEnabled(boolean b) {
+    }
+
+    public void setOrientationLocked(boolean b) {
+    }
+
+    public void setCaptureActivity(Class<CaptureAct> captureActClass) {
+
+    }
+}
