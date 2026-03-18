e594571963add6b8c2315622da9b0aa1214175f7
Prathamsj12 <prathmesh.jangam80@gmail.com>
2026-03-18 16:10:40 +0530

    Cursor & Prduction link

--- app/src/main/java/com/example/moofrosty/core/network/Constants.java
+++ app/src/main/java/com/example/moofrosty/core/network/Constants.java
@@ -2,6 +2,12 @@ package com.example.moofrosty.core.network;
 
 public class Constants {
 //    public static final String BASE_URL = "https://moofrosty.ekatta.in/";
+//    Test link
+
     public static final String BASE_URL = "https://test.moofrosty.in/";
+
+//    Production link(Client)
+// public static final String BASE_URL = "https://moofrosty.in/";
+
 
 }
