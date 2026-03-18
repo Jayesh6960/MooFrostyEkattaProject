f50b4d381696e3d0f33764784bb4063f8d892c3c
Prathamsj12 <132184969+Prathamsj12@users.noreply.github.com>
2026-03-18 16:22:50 +0530

    Jira issues 18 03 2026 (#69)
    * Attendance calender
    
    * Loges cheking purpose changes
    
    * Changes Done By Prathmesh in Doc filed
    
    * Gst Mandatory Remove
    
    * Log check Purpose Changes in the code
    
    * lOG CheckPurpose
    
    * Support_Details_Changes04_03_2026
    
    * Store Rename  As the Outlet
    
    * Address Line 1,2 & 3 Added
    
    * Power Switch icon Position changes
    
    * Support Timing & Outlets
    
    * Testing Purpose changes
    
    * Logs check and Realtime OTP
    
    * Any Day as week off
    
    * Cursor & Prduction link

--- app/src/main/java/com/example/moofrosty/core/network/Constants.java
+++ app/src/main/java/com/example/moofrosty/core/network/Constants.java
@@ -2,6 +2,11 @@ package com.example.moofrosty.core.network;
 
 public class Constants {
 //    public static final String BASE_URL = "https://moofrosty.ekatta.in/";
+//    Test link
     public static final String BASE_URL = "https://test.moofrosty.in/";
+//    Production link(Client)
+// public static final String BASE_URL = "https://moofrosty.in/";
+
+
 
 }
